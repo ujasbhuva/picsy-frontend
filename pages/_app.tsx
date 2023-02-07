b11bc142bc66858a40ee904d7910d199c4f68b27
ujasbhuva <ujasbhuva444@gmail.com>
2023-02-08 01:16:21 +0530

    Adding google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,46 @@
-import '../styles/globals.css'
-import { Provider } from 'react-redux'
-import logger from 'redux-logger'
-import { configureStore } from '@reduxjs/toolkit'
-import reducers from '../store/reducers'
-import Main from '../components/main'
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import { configureStore } from "@reduxjs/toolkit";
+import reducers from "../store/reducers";
+import Main from "../components/main";
+import Script from "next/script";
 
-const preloadedState = {}
+const preloadedState = {};
 const store = configureStore({
   reducer: reducers,
   middleware: (getDefaultMiddleware: any) =>
-    process.env.NEXT_PUBLIC_ENV !== 'prod'
+    process.env.NEXT_PUBLIC_ENV !== "prod"
       ? getDefaultMiddleware().concat(logger)
       : getDefaultMiddleware(),
-  devTools: process.env.NEXT_PUBLIC_ENV !== 'prod',
-  preloadedState
-})
+  devTools: process.env.NEXT_PUBLIC_ENV !== "prod",
+  preloadedState,
+});
 
-export default function App (props: any) {
-
+export default function App(props: any) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-KNQGMWCWXP"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-KNQGMWCWXP', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+        }}
+      />
       <Provider store={store}>
         <Main {...props} />
       </Provider>
     </>
-  )
+  );
 }
