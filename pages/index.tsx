9b220f71b0349f52dda7ac2f506cd40fd622ea1b
ujasbhuva <ujasbhuva444@gmail.com>
2023-02-08 20:14:29 +0530

    modified page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/common/layout'
 export default function Index () {
   return (
     <Layout
-      title={`Picsy - AI Art Search`}
+      title={`Picsy - Art Search`}
       description="Picsy contains more than 6 million digital arts created by Midjourney - Image Generative AI. Picsy is the only platform that allows searching of Midjourney generated arts or images. You can collect or download these images using Picsy for general objectives including research, education, and personal experience."
     >
       <Home />
