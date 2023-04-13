24db28c47ad57bd689f20a573362f09ae7746b19
dipaksavaliya06 <dipak@writesonic.com>
2023-04-13 14:12:27 +0530

    Removed ip filter

--- components/main.tsx
+++ components/main.tsx
@@ -1,60 +1,69 @@
 import {
   CheckCircleIcon,
-  ExclamationCircleIcon
-} from '@heroicons/react/20/solid'
-import { NextComponentType, NextPage, NextPageContext } from 'next'
-import { ReactElement, ReactNode, useEffect, useState } from 'react'
-import { Toaster, resolveValue } from 'react-hot-toast'
-import { getCurrentBrowserFingerPrint } from '@rajesh896/broprint.js'
-import { freeLogin } from '../apiHelper/browser'
-import axios from 'axios'
+  ExclamationCircleIcon,
+} from "@heroicons/react/20/solid";
+import { NextComponentType, NextPage, NextPageContext } from "next";
+import { ReactElement, ReactNode, useEffect, useState } from "react";
+import { Toaster, resolveValue } from "react-hot-toast";
+import { getCurrentBrowserFingerPrint } from "@rajesh896/broprint.js";
+import { freeLogin } from "../apiHelper/browser";
+import axios from "axios";
 
 export type NextPageWithLayout<P = {}> = NextPage<P, P> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 interface MainProps {
-  Component: NextComponentType<NextPageContext, any, {}> & NextPageWithLayout
-  pageProps: any
+  Component: NextComponentType<NextPageContext, any, {}> & NextPageWithLayout;
+  pageProps: any;
 }
 
 const Main: React.FC<MainProps> = ({ Component, pageProps }) => {
-  const [showChild, setShowChild] = useState(false)
+  const [showChild, setShowChild] = useState(false);
+  const [IPv4, setIPv4] = useState<string>("");
+  const [fp, setFp] = useState<string>("");
+  const [country, setCountry] = useState<string>("");
 
   useEffect(() => {
-    setShowChild(true)
-  }, [])
+    setShowChild(true);
+  }, []);
 
   useEffect(() => {
     getCurrentBrowserFingerPrint().then(async (fingerprint: string) => {
-      const res = await axios.get('https://geolocation-db.com/json/')
-      let country = res.data.country_name
-      let ip = res.data.IPv4
-      await freeLogin({
-        browser_token: fingerprint,
+      const res = await axios.get("https://geolocation-db.com/json/");
+      setCountry(res.data.country_name);
+      setIPv4(res.data.IPv4);
+      setFp(fingerprint);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (fp) {
+      freeLogin({
+        browser_token: fp,
         client_country: country,
-        client_ip: ip
-      })
-    })
-  }, [])
+        client_ip: IPv4,
+      }).then((data) => {});
+    }
+  }, [fp, country, IPv4]);
 
   if (!showChild) {
-    return null
+    return null;
   } else {
     return (
       <>
-        <Toaster position='bottom-left'>
-          {t => (
+        <Toaster position="bottom-left">
+          {(t) => (
             <>
-              {t.type === 'success' && (
-                <div className='bg-blue-2 text-white px-4 py-2 gap-2 rounded-lg flex items-center'>
-                  <CheckCircleIcon className='w-6 h-6' />
+              {t.type === "success" && (
+                <div className="bg-blue-2 text-white px-4 py-2 gap-2 rounded-lg flex items-center">
+                  <CheckCircleIcon className="w-6 h-6" />
                   {resolveValue(t.message, t)}
                 </div>
               )}
-              {t.type === 'error' && (
-                <div className='bg-red-400 text-white px-4 py-2 gap-2 rounded-lg flex items-center'>
-                  <ExclamationCircleIcon className='w-6 h-6' />
+              {t.type === "error" && (
+                <div className="bg-red-400 text-white px-4 py-2 gap-2 rounded-lg flex items-center">
+                  <ExclamationCircleIcon className="w-6 h-6" />
                   {resolveValue(t.message, t)}
                 </div>
               )}
@@ -63,8 +72,8 @@ const Main: React.FC<MainProps> = ({ Component, pageProps }) => {
         </Toaster>
         <Component {...pageProps} />
       </>
-    )
+    );
   }
-}
+};
 
-export default Main
+export default Main;
